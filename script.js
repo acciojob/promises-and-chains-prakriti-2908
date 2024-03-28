198dbaea8dcdc2f17a2c286795f7e00749be7ed1
prakriti-2908 <77171197+prakriti-2908@users.noreply.github.com>
2024-03-28 23:18:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,46 @@
 //your JS code here. If required.
+let form = document.getElementById("simpleForm");
+
+async function validateForm(e) {
+	// e.preventDefault();
+  try{  
+	  let x =  document.forms["myForm"]["fname"].value;
+  let y =  document.forms["myForm"]["fage"].value;
+  if (x == "") {
+    alert("Name must be filled out");
+    return false;
+  }
+  if (y == "") {
+    alert("Age must be filled out");
+    return false;
+  }
+	else{
+		let response = await resolvePromise(x,y);
+		alert(response);
+	}
+  }
+	catch(e){
+		alert(e);
+	}
+}
+
+function resolvePromise(name,age){
+	let condition;
+	if(age>18){
+		condition = true;
+	}else{
+		condition = false;
+	}
+	return new Promise((resolve,reject)=>{
+		let id = setTimeout(()=>{
+			clearTimeout(id);
+			if(condition){
+				resolve(`Welcome, ${name}, You can vote.`);
+			}
+			else{
+				reject(`Oh sorry ${name}, You aren't old enough.`);
+			}
+		},4000);
+	});
+}
+
